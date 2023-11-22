eb1674e8dafa2c930ed62e39cea59efccb731039
eunbime <ebk1014@gmail.com>
2023-11-22 15:10:10 +0900

    firebase 파일 수정

--- src/shared/Layout.jsx
+++ src/shared/Layout.jsx
@@ -1,8 +1,8 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
-import styled from "styled-components";
+import { Outlet, useNavigate } from 'react-router-dom'
+import styled from 'styled-components'
 
 function Layout() {
+  const navigate = useNavigate()
   return (
     <div>
       <Header>
@@ -12,7 +12,13 @@ function Layout() {
             <input type="text" placeholder="검색어를 입력해주세요" />
             <button>검색</button>
           </Search>
-          <button>글작성</button>
+          <button
+            onClick={() => {
+              navigate('/write')
+            }}
+          >
+            글작성
+          </button>
           <button>로그인</button>
         </HeaderRight>
       </Header>
@@ -21,38 +27,38 @@ function Layout() {
       </StLayout>
       <Footer>© Corp.</Footer>
     </div>
-  );
+  )
 }
 
-export default Layout;
+export default Layout
 
 const Header = styled.header`
-  width: "100%";
+  width: '100%';
   height: 7rem;
   display: flex;
   align-items: center;
   justify-content: space-between;
   padding: 1rem;
-`;
+`
 
 const Title = styled.h1`
   font-size: xx-large;
   font-weight: bold;
-`;
+`
 
 const HeaderRight = styled.section`
   display: flex;
   gap: 0.5rem;
-`;
+`
 
-const Search = styled.div``;
+const Search = styled.div``
 
 const StLayout = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
   min-height: 90vh;
-`;
+`
 
 const Footer = styled.footer`
   width: 100%;
@@ -61,4 +67,4 @@ const Footer = styled.footer`
   align-items: center;
   justify-content: center;
   font-size: small;
-`;
+`
