77d7b5b0fbe82aa1e706781718762690fb9fdf68
eunbime <ebk1014@gmail.com>
2023-11-25 19:45:41 +0900

    마이페이지 토픽 선택 버튼 및 예외 처리

--- src/pages/Mypage.jsx
+++ src/pages/Mypage.jsx
@@ -1,11 +1,12 @@
-import ProfileBox from "components/ProfileBox";
-import ProfileFigure from "components/ProfileFigure";
-import { doc, getDoc } from "firebase/firestore";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { setUser } from "redux/config/modules/user";
-import styled from "styled-components";
-import { db } from "../firebase";
+import ProfileBox from 'components/ProfileBox'
+import ProfileFigure from 'components/ProfileFigure'
+import { doc, getDoc } from 'firebase/firestore'
+import { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { setUser } from 'redux/modules/user'
+import styled from 'styled-components'
+import { db } from '../firebase'
 
 /**
  * TODO :
@@ -15,31 +16,51 @@ import { db } from "../firebase";
  * 이미지 수정 버튼 누르면 이미지 업로드
  */
 function Mypage() {
-  const { auth, user } = useSelector(state => state);
-  const loginUserUid = auth.loginUserUid;
-  //const user = useSelector(state => state.user);
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
+  const { auth, user, topics, posts } = useSelector((state) => state)
+  const [userTopics, setUserTopics] = useState([])
+  const navigate = useNavigate()
+  console.log(posts)
+  console.log('user', user)
+  const loginUserUid = auth.loginUserUid // 사용자 UID
 
-  console.log("loginuseruid", loginUserUid);
-  console.log("user", user);
+  useEffect(() => {
+    console.log('userTopics', userTopics)
+  }, [])
 
   useEffect(() => {
     const fetchData = async () => {
-      const docRef = doc(db, "userInfo", loginUserUid);
-      const docSnap = await getDoc(docRef);
+      const docRef = doc(db, 'userInfo', loginUserUid)
+      const docSnap = await getDoc(docRef)
 
       if (docSnap.exists()) {
-        console.log("Document data:", docSnap.data());
-        const userFromDB = docSnap.data();
-        dispatch(setUser({ uid: loginUserUid, ...userFromDB }));
+        console.log('Document data:', docSnap.data())
+        const userFromDB = docSnap.data()
+        dispatch(setUser({ uid: loginUserUid, ...userFromDB }))
       } else {
         // docSnap.data() will be undefined in this case
-        console.log("No such document!");
-        alert("user 정보를 가져오지 못했습니다.")
+        console.log('No such document!')
+        alert('user 정보를 가져오지 못했습니다.')
       }
-    };
-    fetchData();
-  }, [loginUserUid]);
+    }
+    fetchData()
+  }, [loginUserUid])
+
+  // if (posts.uid === user.uid) {
+  //   const filteredTopics = topics.filter(
+  //     (topic) => topic.topicName !== posts.topicName
+  //   )
+  //   setUserTopics(filteredTopics)
+  // }
+
+  const filteredPosts = posts.filter((post) => post.uid === user.uid)
+  console.log(user.uid)
+  console.log(filteredPosts[0].uid)
+
+  filteredPosts.map((post) => {
+    if (userTopics.includes(post.topicName)) return
+    setUserTopics((prev) => [...prev, post.topicName])
+  })
 
   return (
     <PageBody>
@@ -48,29 +69,47 @@ function Mypage() {
         <ProfileBox user={user} />
       </Profile>
       <MyFeedBox>
-        카드 보여주는 부분
+        <TopicSelector>
+          {userTopics.length > 0 ? (
+            userTopics.map((topic) => (
+              <li onClick={() => navigate(`/mypage/${topic}`)}>{topic}</li>
+            ))
+          ) : (
+            <div>{`현재 ${user.name}님의 게시물이 존재하지 않습니다`}</div>
+          )}
+        </TopicSelector>
       </MyFeedBox>
     </PageBody>
   )
 }
 
 const PageBody = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `
 const Profile = styled.div`
-    display: flex;
-    width: 800px;
-    height: 500px;
-    border : 1px solid black;
+  display: flex;
+  width: 800px;
+  height: 500px;
+  border: 1px solid black;
 `
 const MyFeedBox = styled.div`
-    width: 800px;
-    height: 800px;
-    border: 1px solid black;
-
+  max-width: 800px;
+  height: 800px;
+  border: 1px solid black;
 `
 
+const TopicSelector = styled.ul`
+  display: flex;
+  gap: 1rem;
+
+  & li {
+    padding: 0.5rem;
+    background-color: gray;
+    border-radius: 0.5rem;
+  }
+`
 
 export default Mypage
