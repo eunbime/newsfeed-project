e138527b367b59d794f71a4c2cdc3742aaa3cc8c
eunbime <ebk1014@gmail.com>
2023-11-25 13:35:10 +0900

    firebase 수정

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,26 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
-import { getStorage } from 'firebase/storage'
+
+// Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_FB_API_KEY,
+//   authDomain: process.env.REACT_APP_FB_AUTH_DOMAIN,
+//   projectId: process.env.REACT_APP_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_APP_ID,
+// }
+
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyBZ7OBLcoy0DBj8XDpDpKHMLyZW9xeYkyU',
+//   authDomain: 'newsfeed-project-2bb4a.firebaseapp.com',
+//   projectId: 'newsfeed-project-2bb4a',
+//   storageBucket: 'newsfeed-project-2bb4a.appspot.com',
+//   messagingSenderId: '51797644632',
+//   appId: '1:51797644632:web:2b89cd7670287c1c33c12d',
+//   measurementId: 'G-GN5ZDPVQX3',
+// }
 
 const firebaseConfig = {
   apiKey: 'AIzaSyBZ7OBLcoy0DBj8XDpDpKHMLyZW9xeYkyU',
@@ -17,4 +36,3 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
 export const db = getFirestore(app)
-export const storage = getStorage(app)
